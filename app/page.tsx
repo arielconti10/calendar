23929c0a9ed500bd2f58dce54d106c09993fc80d
Ariel Conti <arielconti10@gmail.com>
2023-06-14 22:45:30 +0100

    letsgo?

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,34 @@
-// import Link from "next/link"
-// import { Plus } from 'lucide-react'
-// import { Separator } from "@/components/ui/separator"
-// import { buttonVariants } from "@/components/ui/button"
-// import Calendar from "@/components/calendar"
-// import { getBaseUrl } from "@/lib/getBaseUrl"
+import Link from "next/link"
+import { Plus } from 'lucide-react'
+import { Separator } from "@/components/ui/separator"
+import { buttonVariants } from "@/components/ui/button"
+import Calendar from "@/components/calendar"
+import { getBaseUrl } from "@/lib/getBaseUrl"
 
-// async function getAppointments() {
-//   const baseUrl = getBaseUrl();
-//   const appointments = await fetch(`${baseUrl}/api/appointments`, {
-//     cache: "no-cache",
-//   })
+async function getAppointments() {
+  const baseUrl = getBaseUrl();
+  const appointments = await fetch(`${baseUrl}/api/appointments`, {
+    cache: "no-cache",
+  })
 
-//   return appointments.json()
-// }
+  return appointments.json()
+}
 
 export default async function Page() {
-  // const appointments = await getAppointments()
+  const appointments = await getAppointments()
 
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex flex-1 justify-between">
         <h1>My Appointments</h1>
-        {/* <Link href="/appointments/create" className={buttonVariants({ variant: "default" })}>
+        <Link href="/appointments/create" className={buttonVariants({ variant: "default" })}>
           <Plus className="mr-2 h-4 w-4" /> Add New
-        </Link> */}
+        </Link>
       </div>
-      {/* <Separator />
+      <Separator />
       <div className="w-full">
         <Calendar events={appointments} />
-      </div> */}
+      </div>
     </section >
   )
 }
